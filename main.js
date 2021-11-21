54d752905d35df2866c46a96437dcc3db9626b69
bitueboss <69850346+bitueboss@users.noreply.github.com>
2021-11-21 21:21:03 +0600

    'mrrr'

---
+++ main.js
@@ -0,0 +1,10 @@
+// grab here 
+let btn = document.getElementById("button")
+let slide = document.getElementById("slide")
+
+// action is here 
+
+btn.addEventListener("click" , ()=>{
+    slide.classList.toggle("-translate-x-full")
+    
+})
